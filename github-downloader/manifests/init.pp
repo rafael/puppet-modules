e3e4dd3afede2a9b6457d62e58590398bece00a5
Rafael Chacon <rafaelchacon@gmail.com>
2011-09-25 13:20:09 -0700

    Add a module to download repos from github

---
+++ github-downloader/manifests/init.pp
@@ -0,0 +1,22 @@
+# This definitions assumes that the path where you are going to install the
+# has been already created. 
+
+define github::download($user, $path_to_install, $repo_name, $clone_adress) {
+
+  Exec{
+    path => [ "/bin"
+            , "/usr/bin" ],
+  }
+
+  exec {"download-noomii-repo-for-$folder_name":
+    cwd => $cwd,
+    user => $user,
+    logoutput => true,
+    command => "git clone $clone_adress $folder_name --recursive \
+             && bash -c 'cd $folder_name \
+             && git checkout production'",
+    creates => "$cwd/$folder_name",
+    timeout => 3600,
+    require => Class["git::install"],
+  }
+}
