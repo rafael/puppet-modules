232dbcdb6d0511d1c08887edea595206f8f24f9b
Rafael Chacon <rafaelchacon@gmail.com>
2011-09-25 13:46:49 -0700

    Minor fixes to githut downloader module

--- github/manifests/download.pp
+++ github/manifests/download.pp
@@ -12,10 +12,8 @@ define github::download($user, $path_to_install, $repo_name, $clone_adress) {
     cwd => $cwd,
     user => $user,
     logoutput => true,
-    command => "git clone $clone_adress $folder_name --recursive \
-             && bash -c 'cd $folder_name \
-             && git checkout production'",
-    creates => "$path_to_install/$folder_name",
+    command => "git clone $clone_adress $folder_name --recursive"
+    creates => "$path_to_install/$repo_name",
     timeout => 3600,
     require => Class["git::install"],
   }
