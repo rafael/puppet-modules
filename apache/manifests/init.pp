09dc72b1c146bcf754f8270c48f9b17cb4fb64aa
Rafael Chacon <rafaelchacon@gmail.com>
2011-09-25 11:01:56 -0700

    Remove default site from apache installation

--- apache/manifests/init.pp
+++ apache/manifests/init.pp
@@ -1,3 +1,10 @@
 class apache { 
   include apache::install, apache::service
+
+  # I don't want the default site to be installed
+  file { "/etc/apache2/sites-enabled/000-default":
+    ensure => absent,
+    notify => Class["apache::service"],
+    require => Package["apache2"],
+  }
 }
