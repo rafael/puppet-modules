43ab2299793f510ce844c63c189d701073714709
Rafael Chacon <rafaelchacon@gmail.com>
2011-09-27 21:26:09 -0700

    Fix variable name in exec

--- github/manifests/download.pp
+++ github/manifests/download.pp
@@ -8,7 +8,7 @@ define github::download($user, $path_to_install, $repo_name, $clone_adress) {
             , "/usr/bin" ],
   }
 
-  exec {"download-repo-$folder_name":
+  exec {"download-repo-$repo_name":
     cwd => $cwd,
     user => $user,
     logoutput => true,
